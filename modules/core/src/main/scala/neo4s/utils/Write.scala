f0d21b2fc1afe4bcbbda2ca5d900cc9546bdd57f
Nikita Ryanov <ryanov.nikita@gmail.com>
2021-04-25 11:25:32 +0000

    Reformat with scalafmt 2.7.5

--- modules/core/src/main/scala/neo4s/utils/Write.scala
+++ modules/core/src/main/scala/neo4s/utils/Write.scala
@@ -33,22 +33,22 @@ object Write extends WriteLowerPriority {
   implicit def fromPutNullable[A](implicit put: Put[A]): Write[Option[A]] =
     new Write[Option[A]](value => List(put.unsafeToValueNullable(value)))
 
-  implicit def recordWrite[R <: Symbol, H, T <: HList](
-    implicit H: Lazy[Write[H]],
+  implicit def recordWrite[R <: Symbol, H, T <: HList](implicit
+    H: Lazy[Write[H]],
     T: Lazy[Write[T]]
   ): Write[FieldType[R, H] :: T] =
-    new Write[FieldType[R, H] :: T]({
-      case h :: t => H.value.unsafeSet(h) ::: T.value.unsafeSet(t)
+    new Write[FieldType[R, H] :: T]({ case h :: t =>
+      H.value.unsafeSet(h) ::: T.value.unsafeSet(t)
     })
 }
 
 trait WriteLowerPriority extends WriteLowestPriority { this: Write.type =>
-  implicit def product[H, T <: HList](
-    implicit H: Lazy[Write[H]],
+  implicit def product[H, T <: HList](implicit
+    H: Lazy[Write[H]],
     T: Lazy[Write[T]]
   ): Write[H :: T] =
-    new Write[H :: T]({
-      case h :: t => H.value.unsafeSet(h) ::: T.value.unsafeSet(t)
+    new Write[H :: T]({ case h :: t =>
+      H.value.unsafeSet(h) ::: T.value.unsafeSet(t)
     })
 
   implicit val emptyProduct: Write[HNil] = new Write[HNil](_ => Nil)
@@ -60,35 +60,35 @@ trait WriteLowerPriority extends WriteLowestPriority { this: Write.type =>
 trait WriteLowestPriority {
   implicit val emptyOptionProduct: Write[Option[HNil]] = new Write[Option[HNil]](_ => Nil)
 
-  implicit def ohcons1[H, T <: HList](
-    implicit H: Lazy[Write[Option[H]]],
+  implicit def ohcons1[H, T <: HList](implicit
+    H: Lazy[Write[Option[H]]],
     T: Lazy[Write[Option[T]]],
     N: H <:!< Option[α] forSome { type α }
   ): Write[Option[H :: T]] = {
     def split[A](v: Option[H :: T])(f: (Option[H], Option[T]) => A): A = v.fold(f(None, None))({ case h :: t => f(Some(h), Some(t)) })
 
     new Write[Option[H :: T]](
-      split(_) {
-        case (h, t) => H.value.unsafeSet(h) ::: T.value.unsafeSet(t)
+      split(_) { case (h, t) =>
+        H.value.unsafeSet(h) ::: T.value.unsafeSet(t)
       }
     )
   }
 
-  implicit def ohcons2[H, T <: HList](
-    implicit H: Lazy[Write[Option[H]]],
+  implicit def ohcons2[H, T <: HList](implicit
+    H: Lazy[Write[Option[H]]],
     T: Lazy[Write[Option[T]]]
   ): Write[Option[Option[H] :: T]] = {
     def split[A](v: Option[Option[H] :: T])(f: (Option[H], Option[T]) => A): A = v.fold(f(None, None))({ case h :: t => f(h, Some(t)) })
 
     new Write[Option[Option[H] :: T]](
-      split(_) {
-        case (h, t) => H.value.unsafeSet(h) ::: T.value.unsafeSet(t)
+      split(_) { case (h, t) =>
+        H.value.unsafeSet(h) ::: T.value.unsafeSet(t)
       }
     )
   }
 
-  implicit def ogeneric[A, Repr <: HList](
-    implicit G: Generic.Aux[A, Repr],
+  implicit def ogeneric[A, Repr <: HList](implicit
+    G: Generic.Aux[A, Repr],
     B: Lazy[Write[Option[Repr]]]
   ): Write[Option[A]] =
     new Write[Option[A]](value => B.value.unsafeSet(value.map(G.to)))
